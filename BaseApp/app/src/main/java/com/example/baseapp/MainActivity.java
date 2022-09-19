6be84edee53294a9ce955dd0f7a768cdd567e83a
yukuda <leeyookoo@naver.com>
2022-09-19 23:46:50 +0900

    MainActivity

---
+++ BaseApp/app/src/main/java/com/example/baseapp/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.baseapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button button1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button1 = findViewById(R.id.button1);
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "버튼을 눌렀어요",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+}
