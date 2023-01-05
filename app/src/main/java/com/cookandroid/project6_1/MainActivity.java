07acb9370ce14321a7d33299cad1fc28f2450f3f
yukuda <leeyookoo@naver.com>
2023-01-05 23:34:18 +0900

    Project6_1

---
+++ app/src/main/java/com/cookandroid/project6_1/MainActivity.java
@@ -0,0 +1,117 @@
+package com.cookandroid.project6_1;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.os.SystemClock;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CalendarView;
+import android.widget.Chronometer;
+import android.widget.RadioButton;
+import android.widget.TextView;
+import android.widget.TimePicker;
+
+import java.util.Calendar;
+
+public class MainActivity extends AppCompatActivity {
+
+    // (1) 전역변수 선언하기: activity_main.xml에서 id를 부여한 12개 위젯에 대응할 위젯 변수 12개
+    Chronometer chrono;
+    Button btnStart, btnEnd;
+    RadioButton rdoCal, rdoTime;
+    CalendarView calView;
+    TimePicker tPicker;
+    TextView tvYear, tvMonth, tvDay, tvHour, tvMinute;
+    int selectYear, selectMonth, selectDay;
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setIcon(R.drawable.ic_launcher);
+
+        setTitle("시간 예약");
+
+        // (2) onCreate( ) 내부에 코딩: 위젯 변수 12개에 위젯 대입 – 처음에는 캘린더뷰와 타임피커가 보이지 않도록 설정
+        // 버튼
+        btnStart = (Button) findViewById(R.id.btnStart);
+        btnEnd = (Button) findViewById(R.id.btnEnd);
+
+        // 크로노미터
+        chrono = (Chronometer) findViewById(R.id.chronometer1);
+
+        // 라디오버튼 2개
+        rdoCal = (RadioButton) findViewById(R.id.rdoCal);
+        rdoTime = (RadioButton) findViewById(R.id.rdoTime);
+
+        // FrameLayout의 2개 위젯
+        tPicker = (TimePicker) findViewById(R.id.timePicker1);
+        calView = (CalendarView) findViewById(R.id.calendarView1);
+
+        // 텍스트뷰 중에서 연,월,일,시,분 숫자
+        tvYear = (TextView) findViewById(R.id.tvYear);
+        tvMonth = (TextView) findViewById(R.id.tvMonth);
+        tvDay = (TextView) findViewById(R.id.tvDay);
+        tvHour = (TextView) findViewById(R.id.tvHour);
+        tvMinute = (TextView) findViewById(R.id.tvMinute);
+
+        // 처음에는 2개를 안보이게 설정
+        tPicker.setVisibility(View.INVISIBLE);
+        calView.setVisibility(View.INVISIBLE);
+
+        // (3) onCreate( ) 내부에 계속 코딩: 클릭 이벤트 리스너 작성 : 라디오버튼을 클릭하면 캘린더뷰와 타임피커 중 하나씩만 보임
+        rdoCal.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                tPicker.setVisibility(View.INVISIBLE);
+                calView.setVisibility(View.VISIBLE);
+            }
+        });
+
+        rdoTime.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                tPicker.setVisibility(View.VISIBLE);
+                calView.setVisibility(View.INVISIBLE);
+            }
+        });
+
+        // (4-a) onCreate( ) 내부에 계속 코딩: 클릭 이벤트 리스너 작성
+        // 타이머 설정
+        btnStart.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                chrono.setBase(SystemClock.elapsedRealtime());
+                chrono.start();
+                chrono.setTextColor(Color.RED);
+            }
+        });
+
+        // 버튼을 클릭하면 날짜,시간을 가져온다.
+        btnEnd.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                chrono.stop();
+                chrono.setTextColor(Color.BLUE);
+
+                // (5) 예약완료 클릭하면 텍스트뷰에 반영
+                tvYear.setText(Integer.toString(selectYear));
+                tvMonth.setText(Integer.toString(selectMonth));
+                tvDay.setText(Integer.toString(selectDay));
+                tvHour.setText(Integer.toString(tPicker.getCurrentHour()));
+                tvMinute.setText(Integer.toString(tPicker.getCurrentMinute()));
+            }
+        });
+
+        // (4-b) setOnDateChangeListner 설정
+        calView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(CalendarView view, int year, int month, int dayOfMonth) {
+                selectYear =  year;
+                selectMonth = month + 1;
+                selectDay = dayOfMonth;
+            }
+        });
+
+    }
+
+}
